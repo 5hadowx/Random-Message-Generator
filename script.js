1acf84f6ac857381b9672910a2e51f35b5746ba7
5hadowx <soubesda92@gmail.com>
2023-10-24 17:22:23 +0200

    added the messages for the random generator. Also added the messageGenerator function which prints to the console the three lines of a message that will randomly pick a word for zodiac signs, the day's luck and the person's main trait

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('This is a random message generator');
+console.log('Random message of the day:');
+
+const messages = {
+    signs: ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo',
+    'Libra', 'Scorpio', 'Saggitarius', 'Capricorn', 'Aquarius', 'Pisces'],
+    luck: ['good', 'bad', 'excellent', 'terrible'],
+    traits: ['social', 'serious', 'independent', 'affectionate', 'optimistic', 
+    'passionate', 'extroverted', 'deep', 'imaginative', 'stubborn', 'loyal', 'fiery']
+}
+
+const messageGenerator = () => {
+    const messageSign = messages.signs[Math.floor(Math.random() * 12 - 1)];
+    const messageLuck = messages.luck[Math.floor(Math.random() * messages.luck.length - 1)];
+    const messageTrait = messages.traits[Math.floor(Math.random() * messages.traits.length - 1)];
+    console.log(`Your sign is ${messageSign}.`);
+    console.log(`Today you will have ${messageLuck} luck.`);
+    console.log(`Your main trait is that you are ${messageTrait}.`);
+}
+
+messageGenerator();
